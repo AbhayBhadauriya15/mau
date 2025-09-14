0703051d6b69ab8315258f1944534631e1c6e68a
AbhayBhadauriya15 <er.abhaybhadauriya@gmail.com>
2025-09-14 22:56:47 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.44.0"
+    }
+  }
+
+}
+
+provider "azurerm" {
+feature{}
+subscription_id = "3c0d3e39-55e3-464c-b220-26aee31cecb9"
+}
+
+resource "azurerm_resource_group" "mau" {
+  name     = "maurg"
+  location = "West Europe"
+}
+resource "azurerm_storage_account" "mystg" {
+name = "mystg"
+resource_group_name = azurerm_resource_group.mau.name
+location = azurerm_resource_group.mau.location
+account_tier = "standard"
+account_replication_type = "LRS"
+}
+
+
+}
